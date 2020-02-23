e326b65614a84847fe9ec97a57ae60e619b42691
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-02-23 11:09:18 +0100

    setting basic components and routes for email password auth

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -11,7 +11,8 @@ export class AppRoot {
       <ion-app>
         <ion-router useHash={false}>
           <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
+          <ion-route url="/signin-emailpwd" component="auth/app-emailpwd-signin" />
+          <ion-route url="/signup-emailpwd" component="auth/app-emailpwd-signup" />
         </ion-router>
         <ion-nav />
       </ion-app>
