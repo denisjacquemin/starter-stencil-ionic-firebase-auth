62ce2ca8c769e558053632a39813a890457d2414
Denis Jacquemin <denis.jacquemin@gmail.com>
2020-02-25 15:49:36 +0100

    file removed

--- src/config/firebase.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA_oBQb-UyOe5681MswpyJnWxcYv4w4XFo",
-    authDomain: "starter-firebase-auth.firebaseapp.com",
-    databaseURL: "https://starter-firebase-auth.firebaseio.com",
-    projectId: "starter-firebase-auth",
-    storageBucket: "starter-firebase-auth.appspot.com",
-    messagingSenderId: "32306416230",
-    appId: "1:32306416230:web:0778f09fb61bc3b75d3ba7"
-} 
